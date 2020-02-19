ffbbfb1989efbf55550700600d045bd1680ff6fa
kutayyaman <41294174+kutayyaman@users.noreply.github.com>
2020-02-19 22:56:17 +0200

    salary calculate app added (c#)

---
+++ odevdeneme/odevdeneme/Form1.cs
@@ -0,0 +1,193 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+/****************************************************************************
+**					SAKARYA ÜNİVERSİTESİ
+**				BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+**				    BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+**				   NESNEYE DAYALI PROGRAMLAMA DERSİ
+**					2014-2015 BAHAR DÖNEMİ
+**	
+**				ÖDEV NUMARASI..........:1
+**				ÖĞRENCİ ADI............:Kutay	
+**				ÖĞRENCİ NUMARASI.......:B171210074
+**              DERSİN ALINDIĞI GRUP...: D
+****************************************************************************/
+
+
+
+
+
+namespace odevdeneme
+{
+	public partial class Form1 : Form
+	{
+		Button bTextOku;
+		RichTextBox richTextBox1;
+		RichTextBox richTextBox2;
+		OpenFileDialog openFileDialog1;
+		SaveFileDialog saveFileDialog1;
+		Label label1;
+		TextBox textBox1;
+		Button bSorgula;
+		KisiselBilgiler[] kayitliKisiler;
+
+
+		public Form1()
+		{
+			kayitliKisiler = new KisiselBilgiler[500];
+			this.Text = "1. ODEV";
+			this.Width = 750;
+			this.Height = 530;
+
+			bTextOku = new Button();
+			richTextBox1 = new RichTextBox();
+			richTextBox2 = new RichTextBox();
+			openFileDialog1 = new OpenFileDialog();
+			saveFileDialog1 = new SaveFileDialog();
+			label1 = new Label();
+			textBox1 = new TextBox();
+			bSorgula = new Button();
+
+			bTextOku.Text = "TEXT OKU";
+			bTextOku.Top = 150;
+			bTextOku.Left = 500;
+
+			this.Controls.Add(bTextOku);
+
+			richTextBox1.Top = 10;
+			richTextBox1.Left = 10;
+			richTextBox1.Width = 490;
+			richTextBox1.Height = 300;
+
+			this.Controls.Add(richTextBox1);
+
+			richTextBox2.Top = 350;
+			richTextBox2.Left = 10;
+			richTextBox2.Width = 490;
+			richTextBox2.Height = 120;
+
+			this.Controls.Add(richTextBox2);
+
+			label1.Top = 320;
+			label1.Left = 10;
+			label1.Width = 30;
+			label1.Text = "TC";
+
+			this.Controls.Add(label1);
+
+			textBox1.Top = 320;
+			textBox1.Left = 50;
+
+			this.Controls.Add(textBox1);
+
+
+			bSorgula.Top = 320;
+			bSorgula.Left = 170;
+			bSorgula.Text = "SORGULA";
+
+			this.Controls.Add(bSorgula);
+
+			bTextOku.Click += BTextOku_Click;
+			bSorgula.Click += BSorgula_Click;
+			textBox1.KeyPress += TextBox1_KeyPress;
+		}
+
+		private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			// 48 57 8
+			int x = Convert.ToInt32(e.KeyChar);
+			if ((x <= 57 && x >= 48) || x == 8) // tc sayı dışında bir şey olamayacagı icin sayı dışında karakter girişini yasaklamak icin
+			{                                   // ayrıca backspace tusuna basarak yazdiğini silebilmesi icin kosullar
+				e.Handled = false;
+			}
+			else
+			{
+				e.Handled = true;
+				MessageBox.Show("SAYI GIRINIZ", "uygun olmayan karakter", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+			}
+		}
+
+		private void BSorgula_Click(object sender, EventArgs e)
+		{
+			
+			int i = 0; //foreach dongusunun icinde kayitli kisileri tek tek atayabilmek icin
+			if (textBox1.Text != "")  //bos olup olmadıgını kontrol etmek icin
+			{
+				string[] satirlar = null;
+				
+		
+				string hepsi = richTextBox1.Text;
+
+				satirlar = hepsi.Split('\n');
+				string[] kontrol = satirlar[0].Split(' ');
+				if (kontrol.Length == 15)
+				{
+					foreach (string s in satirlar)
+					{
+						string[] kelimeler = s.Split(' ');
+
+						kayitliKisiler[i] = new KisiselBilgiler();               //butun calisanlarin bilgisini tutmus olmak icin
+						kayitliKisiler[i].Tc = Convert.ToString(kelimeler[0]);
+						kayitliKisiler[i].Adi = Convert.ToString(kelimeler[1]);
+						kayitliKisiler[i].SoyAdi = Convert.ToString(kelimeler[2]);
+						kayitliKisiler[i].Yas = Convert.ToInt32(kelimeler[3]);
+						kayitliKisiler[i].CalismaSuresi = Convert.ToInt32(kelimeler[4]);
+						kayitliKisiler[i].EvlilikDurumu = Convert.ToChar(kelimeler[5]);
+						kayitliKisiler[i].EsiCalisiyorMu = Convert.ToChar(kelimeler[6]);
+						kayitliKisiler[i].CocukSayisi = Convert.ToInt32(kelimeler[7]);
+						kayitliKisiler[i].TabanMaas = Convert.ToInt32(kelimeler[8]);
+						kayitliKisiler[i].MakamTazminati = Convert.ToInt32(kelimeler[9]);
+						kayitliKisiler[i].IdariGorevTazminati = Convert.ToInt32(kelimeler[10]);
+						kayitliKisiler[i].FazlaMesaiSaati = Convert.ToInt32(kelimeler[11]);
+						kayitliKisiler[i].FazlaMesaiSaatUcreti = Convert.ToInt32(kelimeler[12]);
+						kayitliKisiler[i].VergiMatrahi = Convert.ToInt32(kelimeler[13]);
+						kayitliKisiler[i].ResimYolu = Convert.ToString(kelimeler[14]);
+
+						i++;
+					}
+				}
+				else
+				{
+					MessageBox.Show("DOGRU TEXTI OKUMADINIZ", "UYARI", MessageBoxButtons.OK, MessageBoxIcon.Information);
+				}
+				string richTextBox2Text = "";
+				for(int j=0;j<i;j++)
+				{
+					kayitliKisiler[j].KayitBul(textBox1.Text, ref richTextBox2Text);
+				}
+				richTextBox2.Text = richTextBox2Text;
+				if(richTextBox2Text=="") //yani KayitBul metodu hala richTextBox2Text stringini degistirmediyse kayit bulunamamistir
+				{
+					MessageBox.Show("Aradıgnız Kayıt Bulunamadi", "BULUNAMADI", MessageBoxButtons.OK,MessageBoxIcon.Error);
+				}
+
+			}
+			else //121. satirdaki if' in else si yani bos birakildiysa calisacak
+			{
+				MessageBox.Show("ARAMAK ISTEDIGNIZ TCYI GIRIN", "BOS BIRAKTINIZ", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			
+		}
+
+		private void BTextOku_Click(object sender, EventArgs e)
+		{
+			if (openFileDialog1.ShowDialog() == DialogResult.OK)
+			{
+				richTextBox1.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+			}
+
+
+		}
+	}
+}
---
+++ odevdeneme/odevdeneme/KisiselBilgiler.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace odevdeneme
+{
+	class KisiselBilgiler
+	{
+	 	private string tc;
+		public string Tc { get { return tc; } set { tc = value; } }
+		private string adi;
+		public string Adi { get { return adi; } set { adi = value; } }
+		private string soyAdi;
+		public string SoyAdi { get { return soyAdi; } set { soyAdi = value; } }
+		private int yas;
+		public int Yas { get { return yas; } set { yas = value; } }
+		private int calismaSuresi;
+		public int CalismaSuresi { get { return calismaSuresi; } set { calismaSuresi = value; } }
+		private char evlilikDurumu;
+		public char EvlilikDurumu { get { return evlilikDurumu; } set { evlilikDurumu = value; } }
+		private char esiCalisiyorMu;
+		public char EsiCalisiyorMu { get { return esiCalisiyorMu; } set { esiCalisiyorMu = value; } }
+
+		private int cocukSayisi;
+		public int CocukSayisi { get { return cocukSayisi; } set { cocukSayisi = value; } }
+		private int tabanMaas;
+		public int TabanMaas { get { return tabanMaas; } set { tabanMaas = value; } }
+		private int makamTazminati;
+		public int MakamTazminati { get { return makamTazminati; } set { makamTazminati = value; } }
+		private int idariGorevTazminati;
+		public int IdariGorevTazminati { get { return idariGorevTazminati; } set { idariGorevTazminati = value; } }
+		private int fazlaMesaiSaati;
+		public int FazlaMesaiSaati { get { return fazlaMesaiSaati; } set { fazlaMesaiSaati = value; } }
+		private int fazlaMesaiSaatUcreti;
+		public int FazlaMesaiSaatUcreti { get { return fazlaMesaiSaatUcreti; } set { fazlaMesaiSaatUcreti = value; } }
+		private int vergiMatrahi;
+		public int VergiMatrahi { get { return vergiMatrahi; } set { vergiMatrahi = value; } }
+		private string resimYolu;
+		public string ResimYolu { get { return resimYolu; } set { resimYolu = value; } }
+		double burutMaas ;
+		public double BurutMaas { get { return burutMaas; } set { burutMaas = value; } }
+		double damgaVergisi;
+		public double DamgaVergisi { get { return damgaVergisi; } set { damgaVergisi = value; } }
+		double gelirVergisi;
+		public double GelirVergisi { get { return gelirVergisi; } set { gelirVergisi = value; } }
+		double emekliKesintisi;
+		public double EmekliKesintisi { get { return emekliKesintisi; } set { emekliKesintisi = value; } }
+		double netMaas ;
+		public double NetMaas { get { return netMaas; } set { netMaas = value; } }
+
+		public void KayitBul(string TC,ref string richTextBox2Text)
+		{
+			if(TC==tc) // textBox1'e girilen aranan tcnin text dosyasi icinde olup olmadigini kontrol etmek icin
+			{
+				string s = "";
+				s = "" + Adi + " isimli kisinin";
+				if (EvlilikDurumu == 'H' || EsiCalisiyorMu == 'E')  //gerekli hesaplamalarin kisinin durumuna gore olmasi icin
+				{
+					BurutMaas =TabanMaas + MakamTazminati + IdariGorevTazminati + CocukSayisi * 30 + FazlaMesaiSaati * FazlaMesaiSaatUcreti;
+					richTextBox2Text = "" + s + "\n BURUT MAASI=" +BurutMaas;
+				}
+				else
+				{
+					BurutMaas = TabanMaas +MakamTazminati + IdariGorevTazminati + 200 + CocukSayisi * 30 + FazlaMesaiSaati * FazlaMesaiSaatUcreti;
+					richTextBox2Text = "" + s + "\n BURUT MAASI=" + BurutMaas;
+				}
+
+				DamgaVergisi = BurutMaas * 10 / 100;
+
+				if (VergiMatrahi < 10000)    // kisinin durumuna gore hesaplamalari dogru yapmak icin
+					GelirVergisi =BurutMaas * 15 / 100;
+				else if (VergiMatrahi < 20000)
+					GelirVergisi = BurutMaas * 20 / 100;
+				else if (VergiMatrahi < 30000)
+					GelirVergisi = BurutMaas * 25 / 100;
+				else
+					GelirVergisi = BurutMaas * 30 / 100;
+
+				EmekliKesintisi = BurutMaas * 15 / 100;
+
+				NetMaas = BurutMaas - (EmekliKesintisi + GelirVergisi +DamgaVergisi);
+
+				richTextBox2Text += "" + "\n DAMGA VERGISI=" + DamgaVergisi + "\n GELIR VERGISI=" + GelirVergisi + "\n EMEKLI KESINTISI=" + EmekliKesintisi + "\n NET MAAS=" + NetMaas + "\n RESIM YOLU: " + ResimYolu;
+
+			}
+			
+		}
+		
+	}
+}
